c292166e7774bc1d7a88e545859cf3d1924fb2ae
Rick Lambrechts <rick@rl-webdiensten.nl>
2023-03-23 10:28:09 +0100

    Initial (#1)

---
+++ src/OpenIDConnectServiceProvider.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MinVWS\OpenIDConnectLaravel;
+
+use Illuminate\Support\ServiceProvider;
+
+class OpenIDConnectServiceProvider extends ServiceProvider
+{
+}
