8f3f7c79ddfd4b5d062a235d4824cc27d8145da0
Rick Lambrechts <rick@rl-webdiensten.nl>
2023-10-10 17:38:19 +0200

    chore: add test for JweDecryptInterface binding

---
+++ tests/Feature/JweDecryptInterfaceBindingTest.php
@@ -0,0 +1,60 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MinVWS\OpenIDConnectLaravel\Tests\Feature;
+
+use Jose\Component\KeyManagement\JWKFactory;
+use MinVWS\OpenIDConnectLaravel\Services\JWE\JweDecryptInterface;
+use MinVWS\OpenIDConnectLaravel\Tests\TestCase;
+use OpenSSLCertificate;
+
+use function MinVWS\OpenIDConnectLaravel\Tests\{
+    generateOpenSSLKey,
+    generateX509Certificate,
+    buildJweString,
+    buildExamplePayload
+};
+
+class JweDecryptInterfaceBindingTest extends TestCase
+{
+    /**
+     * @var resource
+     */
+    protected $decryptionKeyResource;
+    protected OpenSSLCertificate $recipient;
+
+
+    public function setUp(): void
+    {
+        [$key, $keyResource] = generateOpenSSLKey();
+        $this->decryptionKeyResource = $keyResource;
+        $this->recipient = generateX509Certificate($key);
+
+        parent::setUp();
+    }
+
+    /**
+     * @throws \JsonException
+     */
+    public function testJweDecrypter(): void
+    {
+        $payload = buildExamplePayload();
+
+        $jwe = buildJweString(
+            payload: $payload,
+            recipient: JWKFactory::createFromX509Resource($this->recipient)
+        );
+
+        $decrypter = $this->app->make(JweDecryptInterface::class);
+        $decryptedData = $decrypter->decrypt($jwe);
+
+        $this->assertSame($payload, $decryptedData);
+    }
+
+
+    protected function getEnvironmentSetUp($app): void
+    {
+        $app['config']->set('oidc.decryption_key_path', stream_get_meta_data($this->decryptionKeyResource)['uri']);
+    }
+}
--- tests/Unit/Services/JWE/JweDecryptServiceTest.php
+++ tests/Unit/Services/JWE/JweDecryptServiceTest.php
@@ -4,28 +4,25 @@ declare(strict_types=1);
 
 namespace MinVWS\OpenIDConnectLaravel\Tests\Unit\Services\JWE;
 
-use Jose\Component\Core\AlgorithmManager;
-use Jose\Component\Core\JWK;
 use Jose\Component\Core\JWKSet;
-use Jose\Component\Encryption\Algorithm\ContentEncryption\A128CBCHS256;
-use Jose\Component\Encryption\Algorithm\KeyEncryption\RSAOAEP;
-use Jose\Component\Encryption\Compression\CompressionMethodManager;
-use Jose\Component\Encryption\Compression\Deflate;
 use Jose\Component\Encryption\JWE;
-use Jose\Component\Encryption\JWEBuilder;
 use Jose\Component\Encryption\JWEDecrypter;
-use Jose\Component\Encryption\Serializer\CompactSerializer;
 use Jose\Component\Encryption\Serializer\JWESerializerManager;
 use Jose\Component\KeyManagement\JWKFactory;
 use JsonException;
 use MinVWS\OpenIDConnectLaravel\Services\JWE\JweDecryptException;
 use MinVWS\OpenIDConnectLaravel\Services\JWE\JweDecryptService;
 use Mockery;
-use OpenSSLAsymmetricKey;
 use OpenSSLCertificate;
-use OpenSSLCertificateSigningRequest;
 use PHPUnit\Framework\TestCase;
-use RuntimeException;
+
+use function MinVWS\OpenIDConnectLaravel\Tests\{
+    generateOpenSSLKey,
+    generateX509Certificate,
+    getJwkFromResource,
+    buildJweString,
+    buildExamplePayload
+};
 
 class JweDecryptServiceTest extends TestCase
 {
@@ -40,14 +37,14 @@ class JweDecryptServiceTest extends TestCase
     {
         parent::setUp();
 
-        [$key, $keyResource] = $this->generateOpenSSLKey();
+        [$key, $keyResource] = generateOpenSSLKey();
         $this->decryptionKeyResource = $keyResource;
 
         $this->decryptionKeySet = new JWKSet([
-            $this->getJwkFromResource($keyResource),
+            getJwkFromResource($keyResource),
         ]);
 
-        $this->x509Certificate = $this->generateX509Certificate($key);
+        $this->x509Certificate = generateX509Certificate($key);
     }
 
     protected function tearDown(): void
@@ -72,9 +69,9 @@ class JweDecryptServiceTest extends TestCase
      */
     public function testJweDecryption(): void
     {
-        $payload = $this->buildExamplePayload();
+        $payload = buildExamplePayload();
 
-        $jwe = $this->buildJweString(
+        $jwe = buildJweString(
             payload: $payload,
             recipient: JWKFactory::createFromX509Resource($this->x509Certificate)
         );
@@ -95,13 +92,13 @@ class JweDecryptServiceTest extends TestCase
         $this->expectExceptionMessage('Failed to decrypt JWE');
 
         // Create different key
-        [$key, $keyResource] = $this->generateOpenSSLKey();
-        $jwk = $this->getJwkFromResource($keyResource);
+        [$key, $keyResource] = generateOpenSSLKey();
+        $jwk = getJwkFromResource($keyResource);
         $decryptionKeySet = new JWKSet([$jwk]);
 
         // Build JWE for default certificate
-        $payload = $this->buildExamplePayload();
-        $jwe = $this->buildJweString(
+        $payload = buildExamplePayload();
+        $jwe = buildJweString(
             payload: $payload,
             recipient: JWKFactory::createFromX509Resource($this->x509Certificate)
         );
@@ -152,26 +149,26 @@ class JweDecryptServiceTest extends TestCase
      */
     public function testJweDecryptionWithMultipleKeysInKeySet(): void
     {
-        [$firstRecipientKey, $firstRecipientKeyResource] = $this->generateOpenSSLKey();
-        $firstRecipient = $this->generateX509Certificate($firstRecipientKey);
+        [$firstRecipientKey, $firstRecipientKeyResource] = generateOpenSSLKey();
+        $firstRecipient = generateX509Certificate($firstRecipientKey);
 
-        [$secondRecipientKey, $secondRecipientKeyResource] = $this->generateOpenSSLKey();
-        $secondRecipient = $this->generateX509Certificate($secondRecipientKey);
+        [$secondRecipientKey, $secondRecipientKeyResource] = generateOpenSSLKey();
+        $secondRecipient = generateX509Certificate($secondRecipientKey);
 
-        $payload = $this->buildExamplePayload();
+        $payload = buildExamplePayload();
 
-        $firstJwe = $this->buildJweString(
+        $firstJwe = buildJweString(
             payload: $payload,
             recipient: JWKFactory::createFromX509Resource($firstRecipient)
         );
-        $secondJwe = $this->buildJweString(
+        $secondJwe = buildJweString(
             payload: $payload,
             recipient: JWKFactory::createFromX509Resource($secondRecipient)
         );
 
         $jweDecryptService = new JweDecryptService(new JWKSet([
-            $this->getJwkFromResource($firstRecipientKeyResource),
-            $this->getJwkFromResource($secondRecipientKeyResource),
+            getJwkFromResource($firstRecipientKeyResource),
+            getJwkFromResource($secondRecipientKeyResource),
         ]));
 
         // Check if first jwe can be decrypted with key set
@@ -184,103 +181,5 @@ class JweDecryptServiceTest extends TestCase
 
         fclose($firstRecipientKeyResource);
         fclose($secondRecipientKeyResource);
-    }
-
-    protected function buildJweString(string $payload, JWK $recipient): string
-    {
-        // Create the JWE builder object
-        $jweBuilder = new JWEBuilder(
-            new AlgorithmManager([new RSAOAEP()]),
-            new AlgorithmManager([new A128CBCHS256()]),
-            new CompressionMethodManager([new Deflate()])
-        );
-
-        // Build the JWE
-        $jwe = $jweBuilder
-            ->create()
-            ->withPayload($payload)
-            ->withSharedProtectedHeader([
-                'alg' => 'RSA-OAEP',
-                'enc' => 'A128CBC-HS256',
-                'zip' => 'DEF',
-            ])
-            ->addRecipient($recipient)
-            ->build();
-
-        // Get the compact serialization of the JWE
-        return (new CompactSerializer())->serialize($jwe, 0);
-    }
-
-    /**
-     * @throws JsonException
-     */
-    protected function buildExamplePayload(): string
-    {
-        return json_encode([
-            'iat' => time(),
-            'nbf' => time(),
-            'exp' => time() + 3600,
-            'iss' => 'My service',
-            'aud' => 'Your application',
-        ], JSON_THROW_ON_ERROR);
-    }
-
-    /**
-     * Generate OpenSSL Key and return the tempfile resource
-     * @return array{OpenSSLAsymmetricKey, resource}
-     */
-    protected function generateOpenSSLKey(): array
-    {
-        $file = tmpfile();
-        if (!is_resource($file)) {
-            throw new RuntimeException('Could not create temporary file');
-        }
-
-        $key = openssl_pkey_new([
-            'private_key_bits' => 512,
-            'private_key_type' => OPENSSL_KEYTYPE_RSA,
-        ]);
-        if (!$key instanceof OpenSSLAsymmetricKey) {
-            throw new RuntimeException('Could not generate private key');
-        }
-
-        openssl_pkey_export($key, $privateKey);
-        fwrite($file, $privateKey);
-
-        return [$key, $file];
-    }
-
-    /**
-     * Generate X509 certificate
-     * @param OpenSSLAsymmetricKey $key
-     * @return OpenSSLCertificate
-     */
-    protected function generateX509Certificate(OpenSSLAsymmetricKey $key): OpenSSLCertificate
-    {
-        $csr = openssl_csr_new([], $key);
-        if (!$csr instanceof OpenSSLCertificateSigningRequest) {
-            throw new RuntimeException('Could not generate CSR');
-        }
-
-        $certificate = openssl_csr_sign($csr, null, $key, 365);
-        if (!$certificate instanceof OpenSSLCertificate) {
-            throw new RuntimeException('Could not generate X509 certificate');
-        }
-
-        return $certificate;
-    }
-
-    /**
-     * Get JWK from resource
-     * @param $resource resource
-     * @return JWK
-     */
-    protected function getJwkFromResource($resource): JWK
-    {
-        if (!is_resource($resource)) {
-            throw new RuntimeException('Could not create temporary file');
-        }
-
-        return JWKFactory::createFromKeyFile(stream_get_meta_data($resource)['uri']);
     }
 }
